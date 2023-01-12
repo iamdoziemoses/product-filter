94ad9754252c1b5e60dd240b0981f9eafd083d0f
Moses Chidozie <80219224+dozeman-moses@users.noreply.github.com>
2023-01-12 01:45:33 +0100

    fixed filtered

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Categories from "../categories/Categories";
 import Search from "../search/Search";
 import Product from "./Product";
 import "./ProductList.css";
 import { products as productData } from "../../products-data";
 
+const allCategories = new Set(productData.map((product) => product.category));
+
 const ProductList = () => {
   const [products, setProducts] = useState(productData);
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [categories, setCategories] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, products]);
   return (
     <>
       <div className="header">
@@ -16,7 +33,7 @@ const ProductList = () => {
           </h1>
 
           <div className="--flex-between --flex-dir-column --py">
-            <Search />
+            <Search inputValue={search} handleSearch={handleSearch} />
             <Categories />
           </div>
         </header>
@@ -24,15 +41,19 @@ const ProductList = () => {
 
       <div className="product-container">
         <div className="products container --grid-25 --py2">
-          {products.map((product) => {
-            const { id, title, img, price } = product;
+          {filteredProducts.length === 0 ? (
+            <h2>No Product found</h2>
+          ) : (
+            filteredProducts.map((product) => {
+              const { id, title, img, price } = product;
 
-            return (
-              <div key={id}>
-                <Product title={title} img={img} price={price} />
-              </div>
-            );
-          })}
+              return (
+                <div key={id}>
+                  <Product title={title} img={img} price={price} />
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     </>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Search = () => {
+const Search = ({ inputValue, handleSearch }) => {
   return (
     <div className="--form-control">
-      <input type="text" placeholder="Search products" />
+      <input
+        type="text"
+        placeholder="Search products"
+        value={inputValue}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
